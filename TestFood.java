31535d4814baf9d27602bf4979ec963f9852dc73
eawane <eawane@ivytech.edu>
2026-04-19 01:26:19 -0400

    Create TestFood.java

---
+++ TestFood.java
@@ -0,0 +1,7 @@
+// Test class for FoodItem
+public class TestFood {
+    public static void main(String[] args) {
+        FoodItem item = new FoodItem(101, "Burger", 8.99);
+        System.out.println(item.getDetails());
+    }
+}
