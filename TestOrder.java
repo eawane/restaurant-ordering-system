02bad9641296754f2ec01f354973d4251b8d03b3
eawane <eawane@ivytech.edu>
2026-04-19 01:25:35 -0400

    Create TestOrder.java

---
+++ TestOrder.java
@@ -0,0 +1,7 @@
+// Test class for Order
+public class TestOrder {
+    public static void main(String[] args) {
+        Order order = new Order(1, "Ali", "Hassan");
+        System.out.println(order);
+    }
+}
